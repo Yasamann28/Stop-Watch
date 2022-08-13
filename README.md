a63dbc9f508c2b4e79b08b14126b6d163684b738
Yasamann28 <2ujasnm@gmail.com>
2022-08-13 18:02:59 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-arwcmp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-arwcmp)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import StopWatch from './Components/StopWatch/StopWatch';
+
+export default function App() {
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  );
+}
